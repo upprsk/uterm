c9f6ac023981f175bd415f91c68c1e545611382a
Lucas Ross <lucassilveiraross@gmail.com>
2024-04-18 10:39:54 -0300

    style: :gem: run stylua with new rules

--- lua/uterm/utils.lua
+++ lua/uterm/utils.lua
@@ -4,57 +4,57 @@ local U = {}
 ---@param win number Window ID
 ---@return boolean
 function U.is_win_valid(win)
-	return win and vim.api.nvim_win_is_valid(win)
+  return win and vim.api.nvim_win_is_valid(win)
 end
 
 ---Check whether the buffer is valid
 ---@param buf number Buffer ID
 ---@return boolean
 function U.is_buf_valid(buf)
-	return buf and vim.api.nvim_buf_is_loaded(buf)
+  return buf and vim.api.nvim_buf_is_loaded(buf)
 end
 
 ---Convert a given command, which is either a function (that we call here), a
 --string or a list of strings
 ---@param cmd (fun(): (string | string[])) | string | string[]
 function U.make_cmd(cmd)
-	-- if it is a function, call it to get the actual command
-	if type(cmd) == "function" then
-		cmd = cmd()
-	end
+  -- if it is a function, call it to get the actual command
+  if type(cmd) == 'function' then
+    cmd = cmd()
+  end
 
-	-- If it is a table (i.e. {'ls', '-la'}), concatenate to create the actual
-	-- command
-	if type(cmd) == "table" then
-		cmd = table.concat(cmd, " ")
-	end
+  -- If it is a table (i.e. {'ls', '-la'}), concatenate to create the actual
+  -- command
+  if type(cmd) == 'table' then
+    cmd = table.concat(cmd, ' ')
+  end
 
-	-- and here, all that it can be is a string
-	return cmd
+  -- and here, all that it can be is a string
+  return cmd
 end
 
 ---Calculate the dimensions for the floating window using the given ratios.
 ---@param opts TermDimensions
 ---@return { width: integer, height: integer, col: integer, row: integer }
 function U.calc_dimensions(opts)
-	-- get lines and columns
-	local cl = vim.o.columns
-	local ln = vim.o.lines
+  -- get lines and columns
+  local cl = vim.o.columns
+  local ln = vim.o.lines
 
-	-- calculate our floating window size
-	local width = math.ceil(cl * opts.width)
-	local height = math.ceil(ln * opts.height - 4)
+  -- calculate our floating window size
+  local width = math.ceil(cl * opts.width)
+  local height = math.ceil(ln * opts.height - 4)
 
-	-- and its starting position
-	local col = math.ceil((cl - width) * opts.x)
-	local row = math.ceil((ln - height) * opts.y - 1)
+  -- and its starting position
+  local col = math.ceil((cl - width) * opts.x)
+  local row = math.ceil((ln - height) * opts.y - 1)
 
-	return {
-		width = width,
-		height = height,
-		col = col,
-		row = row,
-	}
+  return {
+    width = width,
+    height = height,
+    col = col,
+    row = row,
+  }
 end
 
 return U
